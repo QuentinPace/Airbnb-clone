48471fda0a33821e3a283b76838bffbdaeaaa2a1
mandeepan <mandyphc@gmail.com>
2024-09-18 18:50:02 -0400

    testing

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,19 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
-// backend/routes/api/index.js
+// GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
 
 router.post('/test', function(req, res) {
     //res.cookie('XSRF-TOKEN', req.csrfToken());
