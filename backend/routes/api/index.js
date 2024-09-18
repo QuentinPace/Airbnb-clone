23b3f1cc43b58de8a16c173a29252efeeed73252
QuentinPace <quentinepace@gmail.com>
2024-09-18 19:20:43 -0400

    phase-3 done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,25 +1,10 @@
 // backend/routes/api/index.js
-const router = require('express').Router();
+const router = require("express").Router();
+const { restoreUser } = require("../../utils/auth.js");
 
-// GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
-});
-
-
-router.post('/test', function(req, res) {
-    //res.cookie('XSRF-TOKEN', req.csrfToken());
-    //res.json({made: 'it here'})
-    res.json({ requestBody: req.body });
-  });
-
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+router.use(restoreUser);
 
 module.exports = router;
