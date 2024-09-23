347826e468c885b550a56c634c159ba9e36d72af
mandeepan <mandyphc@gmail.com>
2024-09-23 13:14:48 -0400

    get spots for current user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -40,7 +40,7 @@ router.post(
         }
       }
     });
-
+    
     if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
       const err = new Error('Login failed');
       err.status = 401;
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -10,4 +10,25 @@ router.get('/', async(req, res) => {
     return res.json(allSpots)
 })
 
+router.get('/current', async(req,res) => {
+    console.log("====> 1");
+    const { user } = req;
+    if (user) {
+        console.log("====> 2");
+        const allSpots = await Spot.findAll({
+        where : {ownerId : user.id}
+        })
+        res.status(200)
+        console.log("====> 3");
+        return res.json(allSpots)  
+    } else {
+        console.log("====> 4");
+        res.status(403)
+        return res.json({ user: null });
+    } 
+})
+
+
+
+
 module.exports = router;
