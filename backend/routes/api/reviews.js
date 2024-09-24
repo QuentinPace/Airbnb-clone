f850021e96a0fdb9f2d5e7fb430249fd99d6f74d
mandeepan <mandyphc@gmail.com>
2024-09-24 17:05:34 -0400

    adding image by review id is done

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,6 +7,45 @@ const { Op } = require('sequelize');
 const router = express.Router();
 
 
+router.post('/:reviewId/images',async (req,res)=>{
+    if(!req.user) {
+        return res.json({
+            message: 'Require proper authorization: Review must belong to the current user'
+        })
+    } else{
+        const reviewId = parseInt(req.params.reviewId)
+        const targetReview = await Review.findByPk(reviewId);
+        if (!targetReview){
+            res.status(404);
+            return res.json({
+                "message": "Review couldn't be found"
+            })
+        } else {
+            const existingImages = await ReviewImage.findAll({
+                where : {
+                    reviewId
+                }
+            })
+            if (existingImages.length>=10){
+                res.status(403);
+                res.json({
+                    "message": "Maximum number of images for this resource was reached"
+                })
+            } else {
+                const newReviewImage = await ReviewImage.create({
+                    reviewId,
+                    url:req.body.url
+                })
+                const { id, url } = newReviewImage;
+                res.statusCode = 201;
+                return res.json({id,url});
+            }
+        
+        }
+    }
+})
+
+
 
 
 
