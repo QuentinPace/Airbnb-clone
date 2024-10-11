e20e30f071431b419256d43c402adb13b063a05e
QuentinPace <quentinepace@gmail.com>
2024-10-11 12:46:36 -0400

    logout thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -54,6 +54,14 @@ export const restoreUser = () => async dispatch => {
     dispatch(setUser(data.user))
 }
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE'
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
