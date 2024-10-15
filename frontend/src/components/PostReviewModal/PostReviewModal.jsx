6ab2ef2933e73e67f5a2b464bee7431d04f5cd84
QuentinPace <quentinepace@gmail.com>
2024-10-15 19:29:57 -0400

    basic review implemented, still need to work on interacting with backend and store for review though

--- frontend/src/components/PostReviewModal/PostReviewModal.jsx
+++ frontend/src/components/PostReviewModal/PostReviewModal.jsx
@@ -1,5 +1,43 @@
+import StarRatingInput from "./StarRatingInput"
+import './PostReviewModal.css'
+import { useEffect, useState } from "react";
+
 export default function PostReviewModal() {
+    const [rating, setRating] = useState(0);
+    const [reviewText, setReviewText] = useState('')
+    const [disabled, setDisabled] = useState(true)
+
+    const submitReview = () => {
+        console.log({
+            rating,
+            reviewText
+        })
+    }
+
+    useEffect(() => {
+        if(rating === 0 || reviewText.length < 10) {
+            setDisabled(true)
+        }
+        else{
+            setDisabled(false)
+        }
+    }, [rating, reviewText, setDisabled])
+
+
+    const onChange = (number) => {
+        setRating(parseInt(number));
+    };
+
     return (
-        <h2>post review pwettyy pweez</h2>
+        <>
+            <h1>How was your stay?</h1>
+            <textarea
+            placeholder='Leave your review here...'
+            onChange={e => setReviewText(e.target.value)}/>
+            <StarRatingInput 
+            onChange={onChange}
+            rating={rating}/>
+            <button disabled={disabled} onClick={submitReview}>Submit Your Review</button>
+        </>
     )
 }
---
+++ frontend/src/components/PostReviewModal/StarRatingInput.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { FaStar } from 'react-icons/fa6';
+
+const StarRatingInput = ({ rating, onChange }) => {
+  const [activeRating, setActiveRating] = useState(rating)
+  const starNums = [1, 2, 3, 4, 5]
+
+  const handleClassName = starNumber => {
+    if(starNumber > activeRating){
+      return 'empty'
+    }
+    return 'filled'
+  }
+
+  const propsByStar = starNumber => {
+    return {
+      className: handleClassName(starNumber),
+      key: starNumber,
+      onMouseEnter: e => setActiveRating(starNumber),
+      onMouseLeave: e => setActiveRating(rating),
+      onClick: e => onChange(starNumber)
+    } 
+  }
+
+  return (
+    <>
+      <div className="rating-input">
+        {starNums.map(num => {
+          return (
+            <div {...propsByStar(num)}>
+              <FaStar />
+            </div>
+          )})}
+    </div>
+  </>
+  );
+};
+
+export default StarRatingInput;
